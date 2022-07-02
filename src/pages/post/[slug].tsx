3de9fbb7eeff9730719398202265bd5a49bca261
Felipe Tavares <felipeat07@gmail.com>
2022-07-02 22:46:18 +0100

    add next static props

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,20 +28,43 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return <h1>{post.data.content}</h1>
+}
 
-// export const getStaticPaths = async () => {
+
+// export const getStaticPaths: GetStaticPaths  = async () => {
 //   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+//   const posts = await prismic.getByType('posts');
 
-//   // TODO
+//   return {
+//     paths: [
+      
+//     ],
+//     fallback: false
+//   }
+
 // };
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const prismic = getPrismicClient({});
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('posts', String(params.slug), {})
+
+  const post = {
+    slug: params.slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+    })
+}
+
+  return {
+    props: {
+      response: {post}
+    }
+};
+}
